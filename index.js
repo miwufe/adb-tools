26ca97d8e57937e2dae7fe44efea06071e6bab3f
zhaoyinggang <zhaoyinggang@xiaomi.com>
2021-09-17 17:40:38 +0800

    修改adb源文件

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-module.exports = function (options, next) {
-    var exec = require('child_process').spawn;
+module.exports = function(options, next) {
+  var exec = require("child_process").spawn;
 
-    var adbPath = '';
-    switch (require('os').type()) {
-        case 'Darwin':
-            adbPath = 'mac/adb';
-            break;
-        case 'Linux':
-            adbPath = 'linux/adb';
-            break;
-        default:
-            adbPath = 'win/adb.exe';
-            break;
+  var adbPath = "";
+  switch (require("os").type()) {
+    case "Darwin":
+      adbPath = "mac/adb";
+      break;
+    case "Linux":
+      adbPath = "linux/adb";
+      break;
+    default:
+      adbPath = "win/adb.exe";
+      break;
+  }
+
+  var a = require("path").join(__dirname, adbPath);
+
+  var cmd = [];
+
+  if (typeof options.deviceID === "string") {
+    cmd.push("-s");
+    cmd.push(options.deviceID);
+  }
+  if (typeof options.cmd === "object") {
+    cmd = cmd.concat(options.cmd);
+  }
+
+  var ls = exec(a, cmd);
+  var useNext = false;
+  ls.stdout.on("data", function(data) {
+    useNext = true;
+    next && next(null, data.toString());
+  });
+
+  ls.stderr.on("data", function(data) {
+    next && next(data.toString());
+  });
+
+  ls.on("exit", function() {
+    if (typeof next === "function") {
+      if (useNext === false) {
+        setTimeout(function() {
+          next();
+        }, 200);
+      }
     }
-
-    var a = require('path').join(__dirname, adbPath);
-
-    var cmd = [];
-
-    if (typeof options.deviceID === 'string') {
-        cmd.push('-s');
-        cmd.push(options.deviceID);
-    }
-    if (typeof options.cmd === 'object') {
-        cmd = cmd.concat(options.cmd);
-    }
-
-    var ls = exec(a, cmd);
-    var useNext = false;
-    ls.stdout.on('data', function (data) {
-        //console.log(data.toString());
-        useNext = true;
-        next && next(data.toString());
-    });
-
-
-    ls.stderr.on('data', function (data) {
-        //console.log(data.toString());
-    });
-
-    ls.on('exit', function () {
-        if (typeof next === 'function') {
-            if (useNext === false) {
-                setTimeout(function () {
-                    next();
-                }, 200);
-            }
-        }
-    });
-
+  });
 };
